6469798d62220a1ab21f7725a88baa035e519eda
AshkanMahdizadeh <66483634+AshkanMH1383@users.noreply.github.com>
2024-03-07 07:45:20 +0330

    Add Docker Compose and Folder Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './catalog/product/product.module';
+import { CategoryModule } from './catalog/category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    ProductModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
